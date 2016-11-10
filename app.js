1c9d1b8b426087e483d9079f1c5e39ab6cd65064
Володимир Стрембовський < >
2016-11-10 16:36:33 +0200

    last but not least

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
-(function () {
-'use strict';
+(function (){
+	'use strict';
 
-angular.module('DIApp', [])
-.controller('DIController', DIController);
-
-DIController.$inject = ['$scope', '$filter'];
-function DIController($scope, $filter) {
-  $scope.name = "Yaakov";
-
-  $scope.upper = function () {
-    var upCase = $filter('uppercase');
-    $scope.name = upCase($scope.name);
-  };
-}
-
+	angular.module('LunchCheck', [])
+	.controller('LunchCheckController', LunchCheckController);
+	LunchCheckController.$inject = ['$scope'];
+	function LunchCheckController($scope) {
+		$scope.name="";
+		var comma = ",";
+		function splitString(stringToSplit, separator) {
+			var arrayOfStrings = stringToSplit.split(separator);
+			return arrayOfStrings.length;
+		}
+	 
+	 $scope.checking = function () {
+	 	var lenghtString = splitString($scope.name, comma);
+	 	if($scope.name.length == 0){
+	 		$scope.check= "Please enter data first";
+	 	}
+	 	else if (lenghtString<=3){
+	 		$scope.check= "Enjoy!";
+	 		console.log(lenghtString);
+	 	} else {
+	 		$scope.check= "Too much!";
+	 		console.log(lenghtString);
+	 	}
+ 	
+	 };
+	 	}
+		
+	
 })();
+
+// (function () {
+// 'use strict';
+
+// angular.module('DIApp', [])
+// .controller('DIController', DIController);
+
+// DIController.$inject = ['$scope', '$filter'];
+// function DIController($scope, $filter) {
+//   $scope.name = "Yaakov";
+
+//   $scope.upper = function () {
+//     var upCase = $filter('uppercase');
+//     $scope.name = upCase($scope.name);
+//   };
+// }
+
+// })();
